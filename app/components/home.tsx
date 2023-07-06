44dede95f144ab2fd2b10cc3fd9dfb8509a4a2cb
wk180 <57928552+wk180@users.noreply.github.com>
2023-07-06 11:24:44 +0800

    fixbugs

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -127,7 +127,7 @@ function Screen() {
           config.tightBorder && !isMobileScreen
             ? styles["tight-container"]
             : styles.container
-        } ${getLang() === "ar" ? styles["rtl-screen"] : ""}`
+        } `
       }
     >
       {isAuth ? (
